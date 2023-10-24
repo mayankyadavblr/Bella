2116d21cf534507742b7f64eff9b4e9089888391
mayankyadavblr <prehistoric0001@gmail.com>
2023-10-24 13:28:42 -0400

    working with error handling

--- temp_lexer.py
+++ temp_lexer.py
@@ -34,41 +34,8 @@ tokens = {
     "GT": r'>[^=]',
     "QUESTION": r'\?',
     "COLON": r':',
-    "IGNORE": r' \t'
+    "IGNORE": r'\t|\n|\s'
 }
-
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*[^\*]'
-t_EXPO = r'\*\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_SEMICOLON = r';'
-t_COMMA = r','
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_NOT = r'!'
-t_LT = r'<^='
-t_LE = r'<='
-t_EQ = r'=='
-t_NE = r'!='
-t_GE = r'>='
-t_GT = r'>^='
-t_QUESTION = r'\?'
-t_COLON = r':'
-t_EQUALS = r'='
-t_WHILE = r'\bwhile\b'
-t_LET = r'\blet\b'
-t_FUNCTION = r'\bfunction\b'
-t_TRUE = r'\btrue\b'
-t_FALSE = r'\bfalse\b'
-t_IF = r'\bif\b'
-t_ELSE = r'\belse\b'
-t_PRINT = r'\bprint\b'
-t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_NUMBER = r'\d+'
-t_ignore  = ' \t|\n|\s'
 
 
 def lexer(input, tokenFormats):
@@ -79,21 +46,21 @@ def lexer(input, tokenFormats):
         for tokenFormat in tokenFormats:
             pattern, tag = tokenFormats[tokenFormat], tokenFormat
             regex = re.compile(pattern)
-            match = regex.match(input,pos) #Essentially Build Lexeme
+            match = regex.match(input, pos) #Essentially Build Lexeme
             #print(pattern, tag, match)
             find = False
             if match is not None:
                 lexeme = match.group(0)
+                find = True
 
                 #attr = checkForAttribute(lexeme,tag)
                 if not tag == "IGNORE":
                     token = (lexeme, tag) #removed attr from here
                     tokens.append(token)
-                    find = True
                     break
 
         if not find:
-            print('Illegal or unknown character: %s' % pos)
+            print(f'Illegal or unknown character: {input[pos]}, {pos}')
             pos = pos + 1
         else:
             pos = match.end(0)
